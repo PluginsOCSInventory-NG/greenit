8748bf16d135808d964727be9b8428cc5e02856b
Antoine ROBIN <antoine.robin@factorfx.com>
2023-09-27 13:24:47 +0000

    fix: font size

--- ms_greenit_dashboard/components/computerTypeStats/yesterdayStats.php
+++ ms_greenit_dashboard/components/computerTypeStats/yesterdayStats.php
@@ -8,12 +8,12 @@ echo "<h4>".$l->g(102600)."</h4>";
 $table = '
 <div class="row">
     <div class="col-md-6" style="border: 1px solid #ddd;">
-        <p style="font-size: 2vw; font-weight:bold;">' . (isset($yesterdayData) ? $calculation->ConsumptionFormat($yesterdayData[0]->totalConsumption, "kW/h", $config->CONSUMPTION_ROUND) : '0') . '</p>
-        <p style="color:#333; font-size:1vw;">'.$l->g(102601).'</p>
+        <p style="font-size: 32px; font-weight:bold;">' . (isset($yesterdayData) ? $calculation->ConsumptionFormat($yesterdayData[0]->totalConsumption, "kW/h", $config->CONSUMPTION_ROUND) : '0') . '</p>
+        <p style="color:#333; font-size: 15px;">'.$l->g(102601).'</p>
     </div>
     <div class="col-md-6" style="border: 1px solid #ddd;">
-        <p style="font-size: 2vw; font-weight:bold;">' . (isset($yesterdayData) ? $calculation->TimeFormat($yesterdayData[0]->totalUptime, $config->UPTIME_FORMAT) : '0') . '</p>
-        <p style="color:#333; font-size:1vw;">'.$l->g(102603).'</p>
+        <p style="font-size: 32px; font-weight:bold;">' . (isset($yesterdayData) ? $calculation->TimeFormat($yesterdayData[0]->totalUptime, $config->UPTIME_FORMAT) : '0') . '</p>
+        <p style="color:#333; font-size: 15px;">'.$l->g(102603).'</p>
     </div>
 </div>
 ';
