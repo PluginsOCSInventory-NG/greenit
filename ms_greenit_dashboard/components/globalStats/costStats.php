7800616745bd8faf51502f9b942012bba9888eb7
Antoine ROBIN <antoine.robin@factorfx.com>
2023-10-03 13:00:14 +0000

    refactor: code of global stats

--- ms_greenit_dashboard/components/globalStats/costStats.php
+++ ms_greenit_dashboard/components/globalStats/costStats.php
@@ -5,10 +5,12 @@ echo open_form($form_name, '', '', 'form-horizontal');
 
 echo "<h4>" . $l->g(102700) . "</h4>";
 
+//////////////////////////////
+// Show cost number data
 $table = '
 <div class="row">
     <div class="col-md-4" style="border: 1px solid #ddd; padding: 5px;">
-        <p style="font-size: 32px; font-weight:bold;">' . (isset($yesterdayData) ? $calculation->CostFormat($yesterdayData[0]->totalConsumption, "W/h", $config->KILOWATT_COST, $config->COST_UNIT, $config->COST_ROUND) : '0') . '</p>
+        <p style="font-size: 32px; font-weight:bold;">' . (isset($yesterdayData["GLOBAL"]) && $yesterdayData["GLOBAL"]->totalConsumption != NULL ? $calculation->CostFormat($yesterdayData["GLOBAL"]->totalConsumption, "W/h", $config->KILOWATT_COST, $config->COST_UNIT, $config->COST_ROUND) : '0') . '</p>
         <p style="color:#333; font-size: 15px;">' . $l->g(102701) . '</p>
     </div>
     <div class="col-md-4" style="border: 1px solid #ddd; padding: 5px;">
@@ -34,11 +36,12 @@ $table = '
 ';
 
 echo $table;
+//////////////////////////////
 
 echo "<br>";
 
-// ****************************************** Global stats for collect period  ******************************************/
-
+//////////////////////////////
+// Show cost of device between collect period diagram
 $labels = ["'" . $l->g(102702) . " " . $config->COLLECT_INFO_PERIOD . " " . $l->g(102705) . "'"];
 
 $labelsSettings = array(
@@ -62,9 +65,10 @@ $labelsSettings = array(
 
 $diagram->createCanvas("histogram_collect_period", "6", "200");
 $diagram->createBarChart("histogram_collect_period", "", $labels, $labelsSettings, "bar");
+//////////////////////////////
 
-// ****************************************** Global stats for compare period  ******************************************/
-
+//////////////////////////////
+// Show cost of devices between compare period diagram
 $labels = ["'" . $l->g(102702) . " " . $config->COMPARE_INFO_PERIOD . " " . $l->g(102705) . "'"];
 
 $labelsSettings = array(
@@ -88,6 +92,7 @@ $labelsSettings = array(
 
 $diagram->createCanvas("histogram_compare_period", "6", "200");
 $diagram->createBarChart("histogram_compare_period", "", $labels, $labelsSettings, "bar");
+//////////////////////////////
 
 echo close_form();
 
