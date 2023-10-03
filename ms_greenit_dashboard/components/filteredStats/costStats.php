778ac3403f0e675191a1c1cad11e1a9654b5eed9
Antoine ROBIN <antoine.robin@factorfx.com>
2023-10-03 14:22:23 +0000

    refactor: filtered view code

--- ms_greenit_dashboard/components/filteredStats/costStats.php
+++ ms_greenit_dashboard/components/filteredStats/costStats.php
@@ -5,6 +5,8 @@ echo open_form($form_name, '', '', 'form-horizontal');
 
 echo "<h4>" . $l->g(102700) . "</h4>";
 
+//////////////////////////////
+// Show cost number data
 $table = '
 <div class="row">
     <div class="col-md-4" style="border: 1px solid #ddd; padding: 5px;">
@@ -12,7 +14,7 @@ $table = '
         <p style="color:#333; font-size: 15px;">' . $l->g(102701) . '</p>
     </div>
     <div class="col-md-4" style="border: 1px solid #ddd; padding: 5px;">
-        <p style="font-size: 32px; font-weight:bold;">' . (isset($limitedData) ? $calculation->CostFormat($sumConsumptionInPeriode, "W/h", $config->KILOWATT_COST, $config->COST_UNIT, $config->COST_ROUND) : '0') . '</p>
+        <p style="font-size: 32px; font-weight:bold;">' . (isset($limitedData) ? $calculation->CostFormat($sumConsumptionCollect, "W/h", $config->KILOWATT_COST, $config->COST_UNIT, $config->COST_ROUND) : '0') . '</p>
         <p style="color:#333; font-size: 15px;">' . $l->g(102702) . " " . $config->COLLECT_INFO_PERIOD . " " . $l->g(102705) . '</p>
     </div>
     <div class="col-md-4" style="border: 1px solid #ddd; padding: 5px;">
@@ -23,18 +25,19 @@ $table = '
 ';
 
 echo $table;
+//////////////////////////////
 
 echo "<br>";
 
-// ****************************************** Global stats for collect period  ******************************************/
-
+//////////////////////////////
+// Show cost of device between collect period diagram
 $labels = ["'" . $l->g(102702) . " " . $config->COLLECT_INFO_PERIOD . " " . $l->g(102705) . "'"];
 
 $labelsSettings = array(
     "consumption" => array(
         "backgroundColor" => "'#1941A5'",
         "data" => "[
-            '" . str_replace(" " . "kW/h", "", $calculation->ConsumptionFormat($sumConsumptionInPeriode, "kW/h", $config->CONSUMPTION_ROUND)) . "'
+            '" . str_replace(" " . "kW/h", "", $calculation->ConsumptionFormat($sumConsumptionCollect, "kW/h", $config->CONSUMPTION_ROUND)) . "'
             ]",
         "label" => "'" . $l->g(102706) . " (" . "kW/h" . ")'",
         "type" => "'bar'"
@@ -42,7 +45,7 @@ $labelsSettings = array(
     "cost" => array(
         "backgroundColor" => "'#AFD8F8'",
         "data" => "[
-            '" . str_replace(" " . $config->COST_UNIT, "", $calculation->CostFormat($sumConsumptionInPeriode, "W/h", $config->KILOWATT_COST, $config->COST_UNIT, $config->COST_ROUND)) . "'
+            '" . str_replace(" " . $config->COST_UNIT, "", $calculation->CostFormat($sumConsumptionCollect, "W/h", $config->KILOWATT_COST, $config->COST_UNIT, $config->COST_ROUND)) . "'
             ]",
         "label" => "'" . $l->g(102707) . " (" . $config->COST_UNIT . ")'",
         "type" => "'bar'"
@@ -51,9 +54,10 @@ $labelsSettings = array(
 
 $diagram->createCanvas("histogram_collect_period", "6", "200");
 $diagram->createBarChart("histogram_collect_period", "bar", "", $labels, $labelsSettings);
+//////////////////////////////
 
-// ****************************************** Global stats for compare period  ******************************************/
-
+//////////////////////////////
+// Show cost of devices between compare period diagram
 $labels = ["'" . $l->g(102702) . " " . $config->COMPARE_INFO_PERIOD . " " . $l->g(102705) . "'"];
 
 $labelsSettings = array(
@@ -77,6 +81,7 @@ $labelsSettings = array(
 
 $diagram->createCanvas("histogram_compare_period", "6", "200");
 $diagram->createBarChart("histogram_compare_period", "bar", "", $labels, $labelsSettings);
+//////////////////////////////
 
 echo close_form();
 
