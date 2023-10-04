8a764a8feb7ec68d062ea0ae3188fa5a35a13e6f
Antoine ROBIN <antoine.robin@factorfx.com>
2023-10-04 15:54:49 +0000

    fix: manufacturer view slow loading

--- ms_greenit_dashboard/data/manufacturerStats.php
+++ ms_greenit_dashboard/data/manufacturerStats.php
@@ -1,31 +1,19 @@
 <?php
 
-//////////////////////////////
-// Get yesterday date
 $Date = new DateTime("NOW");
 $Date->modify('-1 day');
-//////////////////////////////
 
-//////////////////////////////
-// Get collect date
 $collectDate = new DateTime("NOW");
 $collectDate->modify("-" . $config->COLLECT_INFO_PERIOD - 1 . " days");
-//////////////////////////////
 
-//////////////////////////////
-// Get compare date
 $compareDate = new DateTime("NOW");
 $compareDate->modify("-" . $config->COMPARE_INFO_PERIOD - 1 . " days");
-//////////////////////////////
 
-//////////////////////////////
-// Create data array
 $manufacturers = array();
+
 $yesterdayData = array();
 $collectData = array();
 $compareData = array();
-//////////////////////////////
-
 
 //////////////////////////////
 // Get manufacturer
@@ -47,62 +35,59 @@ while ($row = mysqli_fetch_object($manufacturersDataResult)) {
 
 //////////////////////////////
 // Get yesterday data of all manufacturer group
-foreach ($manufacturers as $manufacturer) {
-    $yesterdayManufacturerQuery = "
-        SELECT 
-        SUM(greenit.CONSUMPTION) AS totalConsumption, 
-        SUM(greenit.UPTIME) AS totalUptime 
-        FROM greenit 
-        INNER JOIN hardware ON greenit.HARDWARE_ID=hardware.ID 
-        INNER JOIN bios ON greenit.HARDWARE_ID=bios.HARDWARE_ID 
-        WHERE 
-        bios.SMANUFACTURER='$manufacturer'
-        AND greenit.DATE='" . $Date->format("Y-m-d") . "'
-    ";
-    $yesterdayManufacturerDataResult = mysql2_query_secure($yesterdayManufacturerQuery, $_SESSION['OCS']["readServer"]);
+$yesterdayManufacturerQuery = "
+    SELECT 
+    bios.SMANUFACTURER AS MANUFACTURER, 
+    SUM(greenit.CONSUMPTION) AS totalConsumption, 
+    SUM(greenit.UPTIME) AS totalUptime 
+    FROM greenit 
+    INNER JOIN hardware ON greenit.HARDWARE_ID=hardware.ID 
+    INNER JOIN bios ON greenit.HARDWARE_ID=bios.HARDWARE_ID 
+    WHERE 
+    greenit.DATE='" . $Date->format("Y-m-d") . "'
+    GROUP BY MANUFACTURER
+";
+$yesterdayManufacturerDataResult = mysql2_query_secure($yesterdayManufacturerQuery, $_SESSION['OCS']["readServer"]);
 
-    while ($row = mysqli_fetch_object($yesterdayManufacturerDataResult)) {
-        if (
-            $row->totalConsumption != NULL ||
-            $row->totalUptime != NULL
-        ) {
-            $yesterdayData[$manufacturer] = (object) array(
-                "totalConsumption" => $row->totalConsumption,
-                "totalUptime" => $row->totalUptime,
-            );
-        }
+while ($row = mysqli_fetch_object($yesterdayManufacturerDataResult)) {
+    if (
+        $row->totalConsumption != NULL ||
+        $row->totalUptime != NULL
+    ) {
+        $yesterdayData[$row->MANUFACTURER] = (object) array(
+            "totalConsumption" => $row->totalConsumption,
+            "totalUptime" => $row->totalUptime,
+        );
     }
 }
 //////////////////////////////
 
 //////////////////////////////
 // Get collect data of all manufacturer group
-foreach ($manufacturers as $manufacturer) {
-    $collectManufacturerQuery = "
-        SELECT 
-        greenit.DATE, 
-        SUM(greenit.CONSUMPTION) AS totalConsumption, 
-        SUM(greenit.UPTIME) AS totalUptime 
-        FROM greenit 
-        INNER JOIN hardware ON greenit.HARDWARE_ID=hardware.ID 
-        INNER JOIN bios ON greenit.HARDWARE_ID=bios.HARDWARE_ID 
-        WHERE 
-        bios.SMANUFACTURER='$manufacturer'
-        AND greenit.DATE BETWEEN '" . $collectDate->format("Y-m-d") . "' AND '" . $Date->format("Y-m-d") . "'
-        GROUP BY greenit.DATE;
-    ";
-    $collectManufacturerDataResult = mysql2_query_secure($collectManufacturerQuery, $_SESSION['OCS']["readServer"]);
+$collectManufacturerQuery = "
+    SELECT 
+    bios.SMANUFACTURER AS MANUFACTURER, 
+    greenit.DATE, 
+    SUM(greenit.CONSUMPTION) AS totalConsumption, 
+    SUM(greenit.UPTIME) AS totalUptime 
+    FROM greenit 
+    INNER JOIN hardware ON greenit.HARDWARE_ID=hardware.ID 
+    INNER JOIN bios ON greenit.HARDWARE_ID=bios.HARDWARE_ID 
+    WHERE 
+    greenit.DATE BETWEEN '" . $collectDate->format("Y-m-d") . "' AND '" . $Date->format("Y-m-d") . "'
+    GROUP BY greenit.DATE, MANUFACTURER
+";
+$collectManufacturerDataResult = mysql2_query_secure($collectManufacturerQuery, $_SESSION['OCS']["readServer"]);
 
-    while ($row = mysqli_fetch_object($collectManufacturerDataResult)) {
-        if (
-            $row->totalConsumption != NULL ||
-            $row->totalUptime != NULL
-        ) {
-            $collectData[$manufacturer][$row->DATE] = (object) array(
-                "totalConsumption" => $row->totalConsumption,
-                "totalUptime" => $row->totalUptime,
-            );
-        }
+while ($row = mysqli_fetch_object($collectManufacturerDataResult)) {
+    if (
+        $row->totalConsumption != NULL ||
+        $row->totalUptime != NULL
+    ) {
+        $collectData[$row->MANUFACTURER][$row->DATE] = (object) array(
+            "totalConsumption" => $row->totalConsumption,
+            "totalUptime" => $row->totalUptime,
+        );
     }
 }
 
@@ -122,32 +107,30 @@ if (isset($collectData)) {
 
 //////////////////////////////
 // Get compare data of all manufacturer group
-foreach ($manufacturers as $manufacturer) {
-    $compareManufacturerQuery = "
-        SELECT 
-        greenit.DATE, 
-        SUM(greenit.CONSUMPTION) AS totalConsumption, 
-        SUM(greenit.UPTIME) AS totalUptime 
-        FROM greenit 
-        INNER JOIN hardware ON greenit.HARDWARE_ID=hardware.ID 
-        INNER JOIN bios ON greenit.HARDWARE_ID=bios.HARDWARE_ID 
-        WHERE 
-        bios.SMANUFACTURER='$manufacturer'
-        AND greenit.DATE BETWEEN '" . $compareDate->format("Y-m-d") . "' AND '" . $Date->format("Y-m-d") . "'
-        GROUP BY greenit.DATE
-    ";
-    $compareManufacturerDataResult = mysql2_query_secure($compareManufacturerQuery, $_SESSION['OCS']["readServer"]);
+$compareManufacturerQuery = "
+    SELECT 
+    bios.SMANUFACTURER AS MANUFACTURER, 
+    greenit.DATE, 
+    SUM(greenit.CONSUMPTION) AS totalConsumption, 
+    SUM(greenit.UPTIME) AS totalUptime 
+    FROM greenit 
+    INNER JOIN hardware ON greenit.HARDWARE_ID=hardware.ID 
+    INNER JOIN bios ON greenit.HARDWARE_ID=bios.HARDWARE_ID 
+    WHERE 
+    greenit.DATE BETWEEN '" . $compareDate->format("Y-m-d") . "' AND '" . $Date->format("Y-m-d") . "'
+    GROUP BY greenit.DATE, MANUFACTURER
+";
+$compareManufacturerDataResult = mysql2_query_secure($compareManufacturerQuery, $_SESSION['OCS']["readServer"]);
 
-    while ($row = mysqli_fetch_object($compareManufacturerDataResult)) {
-        if (
-            $row->totalConsumption != NULL ||
-            $row->totalUptime != NULL
-        ) {
-            $compareData[$manufacturer][$row->DATE] = (object) array(
-                "totalConsumption" => $row->totalConsumption,
-                "totalUptime" => $row->totalUptime,
-            );
-        }
+while ($row = mysqli_fetch_object($compareManufacturerDataResult)) {
+    if (
+        $row->totalConsumption != NULL ||
+        $row->totalUptime != NULL
+    ) {
+        $compareData[$row->MANUFACTURER][$row->DATE] = (object) array(
+            "totalConsumption" => $row->totalConsumption,
+            "totalUptime" => $row->totalUptime,
+        );
     }
 }
 
