4a8053dde1f7c36fc948af326b0483e211325006
Antoine ROBIN <antoine.robin@factorfx.com>
2023-10-05 07:08:09 +0000

    fix: null value return for filtered view

--- ms_greenit_dashboard/components/filteredStats/yesterdayStats.php
+++ ms_greenit_dashboard/components/filteredStats/yesterdayStats.php
@@ -10,11 +10,11 @@ echo "<h4>" . $l->g(102600) . "</h4>";
 $table = '
 <div class="row">
     <div class="col-md-6" style="border: 1px solid #ddd; padding: 5px;">
-        <p style="font-size: 32px; font-weight:bold;">' . (isset($yesterdayData["FILTERED"]) && $yesterdayData["FILTERED"] != NULL ? $calculation->ConsumptionFormat($yesterdayData["FILTERED"]->totalConsumption, "kW/h", $config->CONSUMPTION_ROUND) : '0') . '</p>
+        <p style="font-size: 32px; font-weight:bold;">' . (isset($yesterdayData["FILTERED"]) && $yesterdayData["FILTERED"]->totalConsumption != NULL ? $calculation->ConsumptionFormat($yesterdayData["FILTERED"]->totalConsumption, "kW/h", $config->CONSUMPTION_ROUND) : '0') . '</p>
         <p style="color:#333; font-size: 15px;">' . $l->g(102601) . '</p>
     </div>
     <div class="col-md-6" style="border: 1px solid #ddd; padding: 5px;">
-        <p style="font-size: 32px; font-weight:bold;">' . (isset($yesterdayData["FILTERED"]) && $yesterdayData["FILTERED"] != NULL ? $calculation->TimeFormat($yesterdayData["FILTERED"]->totalUptime, $config->UPTIME_FORMAT) : '0') . '</p>
+        <p style="font-size: 32px; font-weight:bold;">' . (isset($yesterdayData["FILTERED"]) && $yesterdayData["FILTERED"]->totalUptime != NULL ? $calculation->TimeFormat($yesterdayData["FILTERED"]->totalUptime, $config->UPTIME_FORMAT) : '0') . '</p>
         <p style="color:#333; font-size: 15px;">' . $l->g(102603) . '</p>
     </div>
 </div>
